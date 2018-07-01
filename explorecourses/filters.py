68cf33305dd8fff6e3d99e4dd23b53b80d55edaa
Jeremy Ephron Barenholtz <jeremyephron@gmail.com>
2018-07-01 11:57:48 -0700

    Added search filters and comments
    Added more comprehensive filter support

--- explorecourses/filters.py
+++ explorecourses/filters.py
@@ -2,21 +2,54 @@
 This module contains search filters for the Explore Courses query functionality.
 """
 
+# Term offered
 AUTUMN = "filter-term-Autumn"
 WINTER = "filter-term-Winter"
 SPRING = "filter-term-Spring"
 SUMMER = "filter-term-Summer"
 
+# UG Requirements
 LANGUAGE   = "filter-ger-Language"
 WRITING1   = "filter-ger-Writing1"
 WRITING2   = "filter-ger-Writing2"
 WRITINGSLE = "filter-ger-WritingSLE"
 
-WAY_AII = "filter-ger-WAYAII"
-WAY_AQR = "filter-ger-WAYAQR"
-WAY_CE  = "filter-ger-WAYCE"
-WAY_ED  = "filter-ger-WAYED"
-WAY_ER  = "filter-ger-WAYER"
-WAY_FR  = "filter-ger-WAYFR"
-WAY_SI  = "filter-ger-WAYSI"
-WAY_SMA = "filter-ger-WAYSMA"
+WAY_AII = "filter-ger-WAYAII"  # Aesthetic and Interpretive Inquiry
+WAY_AQR = "filter-ger-WAYAQR"  # Applied Quantitative Reasoning
+WAY_CE  = "filter-ger-WAYCE"   # Creative Expression
+WAY_ED  = "filter-ger-WAYED"   # Engaging Diversity
+WAY_ER  = "filter-ger-WAYER"   # Ethical Reasoning
+WAY_FR  = "filter-ger-WAYFR"   # Formal Reasoning
+WAY_SI  = "filter-ger-WAYSI"   # Social Inquiry
+WAY_SMA = "filter-ger-WAYSMA"  # Scientific Method and Analysis
+
+# Number of units
+UNITS_1   = "filter-units-1"
+UNITS_2   = "filter-units-2"
+UNITS_3   = "filter-units-3"
+UNITS_4   = "filter-units-4"
+UNITS_5   = "filter-units-5"
+UNITS_GT5 = "filter-units-gt5"  # Greater than 5 units
+
+# Time offered
+TIME_EARLY_MORNING = "filter-time-0"  # Before 10am
+TIME_MORNING       = "filter-time-1"  # 10am-12pm
+TIME_LUNCHTIME     = "filter-time-2"  # 12pm-2pm
+TIME_AFTERNOON     = "filter-time-3"  # 2pm-5pm
+TIME_EVENING       = "filter-time-4"  # After 5pm
+
+# Days
+DAY_SUNDAY    = "filter-day-1"
+DAY_MONDAY    = "filter-day-2"
+DAY_TUESDAY   = "filter-day-3"
+DAY_WEDNESDAY = "filter-day-4"
+DAY_THURSDAY  = "filter-day-5"
+DAY_FRIDAY    = "filter-day-6"
+DAY_SATURDAY  = "filter-day-7"
+
+# Career
+CAREER_UG  = "filter-academiclevel-UG"   # Undergraduate
+CAREER_GR  = "filter-academiclevel-GR"   # Graduate
+CAREER_GSB = "filter-academiclevel-GSB"  # Graduate School of Business
+CAREER_LAW = "filter-academiclevel-LAW"  # Law School
+CAREER_MED = "filter-academiclevel-MED"  # Medical School
